f81d62d168c40fa58b8dad65d9da15ff27eb5a82
Gorgeous T <tomisinerinle4@gmail.com>
2025-03-28 13:32:45 -0700

    The random color function created

--- main.js
+++ main.js
@@ -1,34 +1,42 @@
+let newColor; 
+// let newColorHEX; 
+generateColor()
+updateColor()
+console.log($('.__c'))
+
+
+function rgb2hex(r,g,b){
+  return "#" + (1 << 24 | r << 16 | g << 8 | b).toString(16).slice(1);
+}
+
+function generateColor(){
+  const r = Math.floor((Math.random() * (235 - 100 + 1)) + 100);
+  const g = Math.floor((Math.random() * (235 - 100 + 1)) + 100);
+  const b = Math.floor((Math.random() * (235 - 100 + 1)) + 100);
+  newColor = rgb2hex(r, g, b)
+}
+
+
+
+function updateColor(){
+    $('.__c').css({"color": newColor, "border-color":newColor})
+    console.log(newColor)
+    $(".nav-link").hover(function(){
+      $(this).css({"color": newColor,"border-color":newColor});
+      }, function(){$(this).css({"color":"whitesmoke", "border-color":"#648388"});
+    });
+    $(".__h").hover(function(){
+      $(this).css({"color": newColor,"border-color":newColor});
+      }, function(){
+      $(this).css({"color":"#888", "border-color":"#888"});
+    });
+    $('.gif-sec').css({"background-color": newColor})
+    $("#color-swatch").val(newColor)
+}
+
 $(function () {   
-//    let windowSize = $(window).width();
-//    let swatch_arr = [$("#color-swatch_screen"), $("#color-swatch")]
-//    whichSwatch = windowSize >= 768 ? 1 : 0;
-//     $(window).resize(function() {
-//         windowSize = $(window).width();
-//         whichSwatch = windowSize >= 768 ? 1 : 0;
-//         console.log(whichSwatch)
-//     }); 
-    // console.log($(".color-swatch")[whichSwatch])
-    function updateActive(newColor){
-      $('.nav-link.active').css({"color": newColor})
-
-    }
-    
     $("#color-swatch").change(function(){
-        // alert("here")
-        let newColor =  $("#color-swatch").val()
-        $('.__c').css({"color": newColor, "border-color":newColor})
-        $(".nav-link").hover(function(){
-            $(this).css({"color": newColor,"border-color":newColor});
-            }, function(){$(this).css({"color":"whitesmoke", "border-color":"#648388"});
-          });
-        updateActive(newColor)
-        $(".__h").hover(function(){
-            $(this).css({"color": newColor,"border-color":newColor});
-            }, function(){
-            $(this).css({"color":"#888", "border-color":"#888"});
-          });
-        $('.gif-sec').css({"background-color": newColor})
+        newColor =  $("#color-swatch").val()
+        updateColor()
       });
-
-
 });
