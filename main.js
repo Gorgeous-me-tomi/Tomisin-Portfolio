029cb9c5d73f6e524b623de2befd96531513ac64
Gorgeous T <tomisinerinle4@gmail.com>
2025-04-01 04:03:33 -0700

    Got the navSpy working with color changing

--- main.js
+++ main.js
@@ -1,18 +1,22 @@
 let newColor; 
-// let newColorHEX; 
 generateColor()
 updateColor()
-console.log($('.__c'))
 
+const updateNavSpy = () => {
+  $( ".nav-link" ).css("color", "white")
+  $( ".nav-link.active" ).css("color", newColor)
+};
+
+updateNavSpy()
 
 function rgb2hex(r,g,b){
   return "#" + (1 << 24 | r << 16 | g << 8 | b).toString(16).slice(1);
 }
 
 function generateColor(){
-  const r = Math.floor((Math.random() * (235 - 100 + 1)) + 100);
-  const g = Math.floor((Math.random() * (235 - 100 + 1)) + 100);
-  const b = Math.floor((Math.random() * (235 - 100 + 1)) + 100);
+  const r = Math.floor((Math.random() * (235 - 80 + 1)) + 80);
+  const g = Math.floor((Math.random() * (235 - 80 + 1)) + 80);
+  const b = Math.floor((Math.random() * (235 - 80 + 1)) + 80);
   newColor = rgb2hex(r, g, b)
 }
 
@@ -20,15 +24,14 @@ function generateColor(){
 
 function updateColor(){
     $('.__c').css({"color": newColor, "border-color":newColor})
-    console.log(newColor)
     $(".nav-link").hover(function(){
       $(this).css({"color": newColor,"border-color":newColor});
       }, function(){$(this).css({"color":"whitesmoke", "border-color":"#648388"});
     });
     $(".__h").hover(function(){
-      $(this).css({"color": newColor,"border-color":newColor});
+      $(this).css({"color": newColor, "background-color": "white","border-color":newColor});
       }, function(){
-      $(this).css({"color":"#888", "border-color":"#888"});
+      $(this).css({"color": "#888", "background-color":"transparent", "border-color":"#888"});
     });
     $('.gif-sec').css({"background-color": newColor})
     $("#color-swatch").val(newColor)
@@ -39,4 +42,21 @@ $(function () {
         newColor =  $("#color-swatch").val()
         updateColor()
       });
+
+      // toggle gif and img
+    $(".gif-img").click(function(){
+        $("#img-icon").toggle()
+        $("#gif-icon").css("display", "block")
+        $("#gif-img").css("background-image", " url('images/SnakeGame-portfolio.png');")
+        $("#img-icon").css("display", "block")
+        $("#gif-icon").css("display", "none")
+        $("#gif-img").css("background-image", " url('gifs/My_Bitmoji.gif');")
+      alert("The paragraph was clicked.");
+    });
+
+    $(document).scroll(function(){
+      updateNavSpy()
+    })
+
+
 });
